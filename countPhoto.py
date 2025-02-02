7936b253818f4dfa10d7eb8142144f91b96d3241
jezzaayt <gjaytoun@gmail.com>
2025-02-02 16:09:50 +0000

    added some comments

--- countPhoto.py
+++ countPhoto.py
@@ -3,7 +3,6 @@ import polars as pl
 import sys
 from collections import defaultdict
 import matplotlib.pyplot as plt
-import time
 
 def count_files(directory=".", extension=""):
     filenames = []
@@ -16,7 +15,8 @@ def count_files(directory=".", extension=""):
 
 if __name__ == "__main__":
     extensions = ['.raw', '.cr3', '.cr2', '.jpeg', '.png', ".jpg", ".tif", ".tiff", ".bmp", ".svg", 
-    ".gif", ".mp4", ".mov", ".webp",".flv", ".mkv", ".wmv", ".m4v", ".3gp", ".mpg", ".mpeg", ".avi", ".heic", ".heif", ".json"]
+    ".gif", ".mp4", ".mov", ".webp",".flv", ".mkv", ".wmv", ".m4v", ".3gp", ".mpg", ".mpeg", ".avi", ".heic", ".heif", ".json"] # list of extensions to count 
+    # JSON is here as Google Photo exports includes that as part of some photos 
     directory = sys.argv[1] if len(sys.argv) > 1 else os.path.abspath(".")
    # get directory from command line arguments
     print(directory)
@@ -39,22 +39,15 @@ if __name__ == "__main__":
     df.write_csv('photo_video_count.csv')
     if len(df) > 0:
         filtered_df = df.filter(pl.col("count") > 0)
-
-
         fig, ax = plt.subplots()
-    
-     
         ax.scatter(filtered_df['extension'], filtered_df['count']) 
         ax.set_xlabel('Extensions')
         ax.set_ylabel('Count')
         ax.set_title('Scatter plot of Extension by Count')
-
         # Add text labels for each point in the scatter plot
         for i, txt in enumerate(filtered_df['count']):
             ax.annotate(txt, (filtered_df['extension'][i], filtered_df['count'][i]))
-        
         # Set y-axis limits to include all data points within a certain range
         ax.set_ylim([0, max(filtered_df['count']) * 1.2]) # multiply by 1.2 for a bit of extra space at the top
-
         fig.savefig('scatter_plot.png', dpi= 300) 
         
