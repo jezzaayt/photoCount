5cffa1a0545827c5412550a5d697c87afaa5f34b
jezzaayt <gjaytoun@gmail.com>
2025-02-04 18:44:11 +0000

    Refactor: removed main() functions into other smaller functions better

--- countPhoto.py
+++ countPhoto.py
@@ -1,103 +1,79 @@
 import os 
-import polars as pl 
+import polars as pl
 import sys
 import matplotlib.pyplot as plt
-import logging
 from tqdm import tqdm
 
-
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-def count_files(directory=".", extension=""):
-    try:
-        filenames = []
-        for dirpath, dirnames, files in os.walk(directory):
-            for filename in files:
-                if filename.endswith(extension) or filename.endswith(extension.upper()):
-                    filenames.append(os.path.join(dirpath, filename))
 
-        return len(filenames), filenames  # Returns count and list of file names
-    except Exception as e:
-        logging.error(f"Error counting files: {e}")
-        return 0, []
+def count_files(directory=".", extensions=None):
+    if extensions is None:
+        extensions = ['.raw', '.cr3', '.cr2', '.jpeg', '.png', ".jpg", ".tif", ".tiff", ".bmp", ".svg", 
+                      ".gif", ".mp4", ".mov", ".webp", ".flv", ".mkv", ".wmv", ".m4v", ".3gp", ".mpg", 
+                      ".mpeg", ".avi", ".heic", ".heif", ".json"]
 
-def parse_args():
-    if len(sys.argv) > 1:
-        directory = sys.argv[1]
-    else:
-        directory = os.path.abspath(".")
-    if len(sys.argv) > 2:
-        output_csv = sys.argv[2]
-    else:
-        output_csv = 'photo_video_count.csv'
-    
-    extensions = ['.raw', '.cr3', '.cr2', '.jpeg', '.png', ".jpg", ".tif", ".tiff", ".bmp", ".svg", 
-    ".gif", ".mp4", ".mov", ".webp",".flv", ".mkv", ".wmv", ".m4v", ".3gp", ".mpg", ".mpeg", ".avi", ".heic", ".heif", ".json"] # list of extensions to count 
-    # JSON is here as Google Photo exports includes that as part of some photos 
-    return directory, extensions, output_csv
-
-
-def plot_count(df):
- 
-    fig, ax = plt.subplots()
-    ax.scatter(df['extension'], df['count']) 
-    ax.set_xlabel('Extensions')
-    ax.set_ylabel('Count')
-    ax.set_title('Scatter plot of Extension by Count')
-    # Add text labels for each point in the scatter plot
-    for i, txt in enumerate(df['count']):
-        ax.annotate(txt, (df['extension'][i], df['count'][i]))
-    # Set y-axis limits to include all data points within a certain range
-    ax.set_ylim([0, max(df['count']) * 1.2]) # multiply by 1.2 for a bit of extra space at the top
-    fig.savefig('scatter_plot.png', dpi= 300)
-
-def main(directory, extensions, output_csv):
-    
     if not os.path.exists(directory):
         logging.error(f"Directory {directory} does not exist")
-        sys.exit()
+        return 0, []
 
-    
-    file_list = []
-    for ext in tqdm(extensions, desc="Counting files"):
-        count, filenames = count_files(directory, ext.strip('.'))
-        if count > 0:
-            file_list.extend(filenames)
-        
-    df = pl.DataFrame({
-        'filename': file_list,
-        'extension': [os.path.splitext(filename)[1][1:] for filename in file_list]
-
+    extension_counts = pl.DataFrame({
+        'filename': [],
+        'extension': []
     })
-    # Aggregate and count unique extensions
-    extension_counts = (
-        df.lazy()
-        .group_by('extension')
-        .agg(pl.len().alias('count'))
-        .sort('count', descending=True)
+
+    for ext in tqdm(extensions, desc="Counting files"):
+        count = (
+            pl.scan_paths(
+                os.path.join(directory, "**/*." + ext),
+                projection="filename"
+            )
+            .with_column(pl.col('filename').str.extract(r'.*/([^/]+)$').alias('filename'))
+            .with_column(pl.col('filename').str.slice(-len(ext), None).str.strip('.').alias('extension'))
+            .group_by("extension")
+            .agg([pl.count().alias('count')])
+            .collect()
+        )
+
+        if count.shape[0] > 0:
+            extension_counts = extension_counts.vstack(count)
+
+    total_count = sum(extension_counts.get_column('count'))
+    percentage_df = (
+        extension_counts
+        .with_columns(
+            pl.col('count').map_elements(lambda x: f'{(x/total_count)*100:.3f}%', return_dtype=pl.Utf8).alias('percentage')
+        )
         .filter(pl.col("count") > 0)
-    ).collect()
-    print(f"Total files found: {sum(extension_counts['count'])}")
-    print(extension_counts)
-
-    # Add percentage column
-    total_count = extension_counts.get_column('count').sum()
-    extension_counts = extension_counts.with_columns(
-        pl.col('count').map_elements(lambda x: f'{(x/total_count)*100:.3f}%', return_dtype=pl.Utf8).alias('percentage')
     )
-    print(extension_counts)
-#     # Write to CSV
+
+    print(f"Total files found: {sum(extension_counts['count'])}")
+    print(percentage_df)
+
+    if not percentage_df.is_empty():
+        percentage_df.write_csv('photo_video_count.csv')
+
+    return percentage_df
+
+def plot_count(df):
+    fig, ax = plt.subplots()
+    ax.bar(df['extension'], df['count'])
+    ax.set_xlabel('Extensions')
+    ax.set_ylabel('Count')
+    ax.set_title('Bar Plot of File Extensions by Count')
     
-    if not extension_counts.is_empty():
-        extension_counts.write_csv(output_csv)
-        plot_count(extension_counts)
-    if extension_counts.is_empty():
-        logging.info("No files found with specified extensions.")
-        return
+    for i, count in enumerate(df['count']):
+        ax.text(i, count + 50, str(count), ha='center', va='bottom')
 
-
+    plt.tight_layout()
+    plt.savefig('scatter_plot.png', dpi=300)
 
 if __name__ == "__main__":
+    directory = os.path.abspath('.')
+    extensions = ['.raw', '.cr3', '.cr2', '.jpeg', '.png', ".jpg", ".tif", ".tiff", ".bmp", ".svg",
+                  ".gif", ".mp4", ".mov", ".webp", ".flv", ".mkv", ".wmv", ".m4v", ".3gp", ".mpg",
+                  ".mpeg", ".avi", ".heic", ".heif", ".json"]
 
-    directory, extensions, output_csv = parse_args()
-    main(directory, extensions, output_csv)
+    percentage_df = count_files(directory, extensions)
     
+    if not percentage_df.is_empty():
+        plot_count(percentage_df)
